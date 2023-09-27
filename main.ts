6c58464d7a02d900933c123e86390e87d5bb308f
KanyeNorthSouth <145537967+KanyeNorthSouth@users.noreply.github.com>
2023-09-27 10:03:07 +0300

    joku kuva

--- main.ts
+++ main.ts
@@ -1,1 +1,14 @@
-
+input.onButtonPressed(Button.A, function () {
+    for (let index = 0; index < 4; index++) {
+        basic.showIcon(IconNames.Heart)
+        basic.pause(2000)
+        basic.showLeds(`
+            # # # # #
+            # . . . .
+            # # # # #
+            # . . . .
+            # # # # #
+            `)
+        basic.pause(2000)
+    }
+})
